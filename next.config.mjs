604c93dee8626e4ae7435ac6fdc2dfd49ad64b7f
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2024-10-13 15:07:20 +0200

    gh-pages optimisation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import pkg from './package.json'
+
+const nextConfig = {
+  basePath: `/${pkg.name}`,
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
